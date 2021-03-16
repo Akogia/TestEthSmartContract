fadff04ad28a9edb0b4411d7954872807264fc1b
Akogia <66163771+Akogia@users.noreply.github.com>
2021-03-16 15:19:58 +0100

    added interaction to eth using web3

--- src/App.js
+++ src/App.js
@@ -1,8 +1,52 @@
-import logo from './logo512.png';
-import './App.css';
+import logo from './logo512.png'
+import React, {Component} from 'react'
+import './App.css'
+import Web3 from 'web3'
 
-function App() {
-  return (
+class App extends Component {
+
+  async componentWillMount() {
+    await this.loadWeb3()
+    console.log(window.web3)
+    await this.loadBlockchainData()
+  }
+  
+  async loadWeb3() {
+    if(window.ethereum) {
+      window.web3 = new Web3(window.ethereum)
+      await window.ethereum.enable()
+    }
+    else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider)
+    }
+    else {
+      window.alert('Non-Ethereum browser detected. Try MetaMask')
+    }
+  } 
+
+  async loadBlockchainData(){
+    const web3 = window.web3
+    const accounts = await web3.eth.getAccounts()
+    console.log(accounts)
+    this.setState({account: accounts[0]})
+    console.log(this.state.account)
+    const ethBalance = await web3.eth.getBalance(this.state.account)
+    this.setState({ethBalance})
+    console.log(ethBalance)
+  }
+
+  constructor(props){
+    super(props)
+    this.state = {
+      web3: 'undefined',
+      account: '',
+      token: null,
+      ethBalance: 0,
+      BankAddress: null
+    }
+  }
+
+  render () {return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -20,6 +64,7 @@ function App() {
       </header>
     </div>
   );
+  }
 }
 
 export default App;
